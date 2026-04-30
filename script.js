0a3446636ccc0e842a45e24329ff487d6b897713
Reborn4797 <95199911+Reborn4749@users.noreply.github.com>
2026-04-30 22:03:20 +0700

    feat: implement airdrop eligibility logic

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const btn = document.getElementById("checkBtn");
 const input = document.getElementById("walletInput");
 const result = document.getElementById("resultText");
+btn.addEventListener("click", function() {
+    if (input.value === "") {
+        result.textContent = "❌ กรุณากรอก Wallet Address ก่อนครับ!";
+        result.style.color = "red";
+    } else {
+        result.textContent = "🎉 ยินดีด้วย! คุณได้รับสิทธิ์ 500 $BASE 🔵";
+        result.style.color = "green";
+    }
+});
